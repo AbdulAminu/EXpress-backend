4eda4826c3657022cdfbed4bb2cd56e0d8c1abda
AbdulAminu <abdulrasheedkola2002@gmail.com>
2026-04-22 11:51:18 +0100

    new changes

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,10 +3,10 @@ import { userValidaton, userValForLogin } from "../validator/userValidator.js";
 import bcrypt from "bcryptjs";
 import { genToken } from "../utils/tokenGen.js";
 
-export const about=(req, res)=>{
-  res.send(`<h1>This is the about page</h1>`)
-}
-export const signUp= async (req, res) => {
+export const about = (req, res) => {
+  res.send(`<h1>This is the about page</h1>`);
+};
+export const signUp = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
@@ -113,7 +113,7 @@ export const fetchAllUsers = async (req, res) => {
       });
     }
     res.status(200).json({
-      message: "user retrieved successfully",
+      message: "users retrieved successfully 📦",
       data: user,
     });
   } catch (err) {
@@ -132,11 +132,11 @@ export const getSingle = async (req, res) => {
 
     if (!user) {
       return res.status(404).json({
-        message: `User with id: ${id} doesn't exist`,
+        message: `User with ID ${id} does not exist ❌`,
       });
     }
     res.status(200).json({
-      message: "User retrieved succesfully",
+      message: "User retrieved successfully 📦",
       data: user,
     });
   } catch (err) {
@@ -150,14 +150,14 @@ export const deleteUser = async (req, res) => {
   const { id } = req.params;
   try {
     const user = await userModel.findOneAndDelete(id);
-    if(!user){
+    if (!user) {
       res.status(404).json({
-        message:`User with id: ${id} not found`
-      })
+        message: `User with id: ${id} not found`,
+      });
     }
     res.status(200).json({
-      message:"User deleted successdully"
-    })
+      message: "User deleted successfully ✅",
+    });
   } catch (err) {
     if (err instanceof Error) {
       console.error(err);
@@ -165,18 +165,18 @@ export const deleteUser = async (req, res) => {
     }
   }
 };
-export const logOut = async (req, res)=>{
- try{
-   res.clearCookie("genToken", {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: "lax",
-       })
+export const logOut = async (req, res) => {
+  try {
+    res.clearCookie("genToken", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+    });
 
-       return res.status(200).json({
-          message: "Account logout successful"
-       })
- }catch(err){
-  console.error(err)
- }
-}
+    return res.status(200).json({
+      message: "Logged out successfully 👋",
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
