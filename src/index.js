6e906c5b2c5dc6d7c0ee6570369886ee5eb04487
AbdulAminu <abdulrasheedkola2002@gmail.com>
2026-05-16 20:55:28 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://task-cortex.vercel.app/"],
   methods: ["GET", "POST", "DELETE", "PUT"],
   credentials: true,
 }));
